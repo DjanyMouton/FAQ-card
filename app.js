5d565d217269aca4e2643dd3b217ee9cbfddfc22
Djany Mouton <djany.developpement@gmail.com>
2022-12-13 00:36:17 +0100

    Project finished

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 let question = document.querySelectorAll('h3');
 let answer = document.querySelectorAll('.answer');
+let arrow = document.querySelectorAll('.img-arrow')
 
 for(let i = 0; i < question.length; i++) {
     question[i].addEventListener('click', () => {
-        answer[i].style.display == 'flex' ? answer[i].style.display = 'none' : answer[i].style.display = 'flex';
+        if (answer[i].style.display == 'flex') {
+            answer[i].style.display = 'none';
+            answer[i].style.animation = '1s linear';
+            arrow[i].style.rotate = '360deg';
+        } else {
+            answer[i].style.display = 'flex';
+            answer[i].style.animation = '1s linear';
+            arrow[i].style.rotate = '180deg';
+        }
     })
 };
